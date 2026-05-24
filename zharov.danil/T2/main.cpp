f10347aed8d599112f68bb47d13b15d3e54c2730
AxellorWave <Danya.Zharov.2006@gmail.com>
2026-05-24 17:38:11 +0300

    some operators

--- zharov.danil/T2/main.cpp
+++ zharov.danil/T2/main.cpp
@@ -1,5 +1,11 @@
 #include <iostream>
+#include <string>
+#include <vector>
+#include <cassert>
 #include <complex>
+#include <iomanip>
+#include <iterator>
+#include <sstream>
 
 namespace zharov
 {
@@ -26,6 +32,39 @@ namespace zharov
     std::string key3;
   };
 
+  struct DelimiterIO
+  {
+    char exp;
+  };
+
+  struct UllIO
+  {
+    ull_t& ref;
+  };
+
+  struct CmpIO
+  {
+    std::complex< double >& ref;
+  };
+
+  struct ConstCmpIO
+  {
+    const std::complex< double >& ref;
+  };
+
+  struct StringIO
+  {
+    std::string& ref;
+  };
+
+  std::istream& operator>>(std::istream& in, DelimiterIO&& dest);
+  std::istream& operator>>(std::istream& in, UllIO&& dest);
+  std::istream& operator>>(std::istream& in, CmpIO&& dest);
+  std::ostream& operator<<(std::ostream& out, const ConstCmpIO& dest);
+  std::istream& operator>>(std::istream& in, StringIO&& dest);
+  std::istream& operator>>(std::istream& in, DataStruct& dest);
+  std::ostream& operator<<(std::ostream& out, const DataStruct& dest);
+  bool operator<(const DataStruct& lhs, const DataStruct& rhs);
 }
 
 int main()
@@ -45,4 +84,125 @@ zharov::IOGuard::~IOGuard()
   s_.precision(precision_);
   s_.fill(fill_);
   s_.flags(fmt_);
+}
+
+std::istream& zharov::operator>>(std::istream& in, DataStruct& dest)
+{
+  std::istream::sentry sentry(in);
+  if (!sentry)
+  {
+    return in;
+  }
+  DataStruct input;
+  {
+    using sep = DelimiterIO;
+    using ull = UllIO;
+    using cmp = CmpIO;
+    using str = StringIO;
+    std::string key1, key2, key3;
+    in >> sep{'('};
+    in >> sep{':'} >> key1 >> ull{input.key1};
+    in >> sep{':'} >> key2 >> cmp{input.key2};
+    in >> sep{':'} >> key3 >> str{input.key3};
+    in >> sep{':'} >> sep{')'};
+  }
+  if (in)
+  {
+    dest = input;
+  }
+  return in;
+}
+
+std::ostream& zharov::operator<<(std::ostream& out, const DataStruct& src)
+{
+  std::ostream::sentry sentry(out);
+  if (!sentry)
+  {
+    return out;
+  }
+  IOGuard fmtguard(out);
+  out << "(";
+  out << ":\"key1\" 0" << src.key1;
+  out << ":\"key2\" " << ConstCmpIO{src.key2};
+  out << ":\"key3\" " << src.key3;
+  out << ":)";
+  return out;
+}
+
+std::istream& zharov::operator>>(std::istream& in, DelimiterIO&& dest)
+{
+  std::istream::sentry sentry(in);
+  if (!sentry)
+  {
+    return in;
+  }
+  char c = 0;
+  in >> c;
+  if (in && (c != dest.exp))
+  {
+    in.setstate(std::ios::failbit);
+  }
+  return in;
+}
+
+std::istream& zharov::operator>>(std::istream& in, StringIO&& dest)
+{
+  std::istream::sentry sentry(in);
+  if (!sentry)
+  {
+    return in;
+  }
+  return std::getline(in >> DelimiterIO{'"'}, dest.ref, '"');
+}
+
+std::istream& zharov::operator>>(std::istream& in, UllIO&& dest)
+{
+  std::istream::sentry s(in);
+  if (!s)
+  {
+    return in;
+  }
+  double a = 0, b = 0;
+  in >> DelimiterIO{{'0'}};
+  in >> a;
+  dest.ref = a;
+  return in
+}
+
+std::istream& zharov::operator>>(std::istream& in, CmpIO&& dest)
+{
+  std::istream::sentry s(in);
+  if (!s)
+  {
+    return in;
+  }
+  double a = 0, b = 0;
+  in >> DelimiterIO{{'#'}} >> DelimiterIO{{'c'}};
+  in >> DelimiterIO{{'('}} >> a >> b >> DelimiterIO{{')'}};
+  dest.ref = {a, b};
+  return in;
+}
+
+std::ostream& zharov::operator<<(std::ostream& out, const ConstCmpIO& dest)
+{
+  std::ostream::sentry s(out);
+  if (!s)
+  {
+    return out;
+  }
+  double a = 0, b = 0;
+  out << "#c(";
+  out << std::fixed << std::setprecision(1) << dest.ref.real() << ' ' << dest.ref.imag() << ')';
+  return out;
+}
+
+bool zharov::operator<(const DataStruct& lhs, const DataStruct& rhs)
+{
+  bool f = lhs.key1 < rhs.key1;
+  if (!f)
+  {
+    f = f || std::abs(lhs.key2) < std::abs(rhs.key2);
+  }
+  f = f || lhs.key3.length() < rhs.key3.length();
+  return f;
 }
