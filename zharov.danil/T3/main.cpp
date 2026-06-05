9313200bc7a05a3eca0e3f10f59af603e09098bc
AxellorWave <Danya.Zharov.2006@gmail.com>
2026-06-05 20:41:32 +0300

    enter polygons and cmnds with while

--- zharov.danil/T3/main.cpp
+++ zharov.danil/T3/main.cpp
@@ -1,2 +1,65 @@
-int main()
-{}
+#include <algorithm>
+#include <iostream>
+#include <vector>
+#include <fstream>
+#include <functional>
+#include <iterator>
+#include <limits>
+#include <unordered_map>
+#include "commands.hpp"
+#include "polygon.hpp"
+
+int main(int argc, char* argv[])
+{
+  if (argc != 2)
+  {
+    std::cerr << "Bad arg\n";
+    return 1;
+  }
+  std::ifstream file(argv[1]);
+  if (!file)
+  {
+    std::cerr << "Cannot open file\n";
+    return 1;
+  }
+  zharov::data_t polygons;
+  {
+    using iit_t = std::istream_iterator< zharov::Polygon >;
+    std::copy(iit_t{file}, iit_t{}, std::back_inserter(polygons));
+    polygons.erase(
+      std::remove_if(polygons.begin(), polygons.end(), zharov::isInvalid), polygons.end());
+  }
+  using cmd_t = std::function< void(std::istream&, std::ostream&) >;
+  std::unordered_map< std::string, cmd_t > cmds;
+  {
+    using namespace std::placeholders;
+    cmds["AREA"] = std::bind(zharov::handleArea, _1, _2, std::cref(polygons));
+    cmds["MAX"] = std::bind(zharov::handleMax, _1, _2, std::cref(polygons));
+    cmds["MIN"] = std::bind(zharov::handleMin, _1, _2, std::cref(polygons));
+    cmds["COUNT"] = std::bind(zharov::handleCount, _1, _2, std::cref(polygons));
+    cmds["RECTS"] = std::bind(zharov::handleRects, _1, _2, std::cref(polygons));
+    cmds["RIGHTSHAPES"] = std::bind(zharov::handleRightShapes, _1, _2, std::cref(polygons));
+  }
+  std::string cmd;
+  while (std::cin >> cmd)
+  {
+    auto it = cmds.find(cmd);
+    if (it == cmds.end())
+    {
+      std::cout << "<INVALID COMMAND>\n";
+      std::cin.ignore(std::numeric_limits< std::streamsize >::max(), '\n');
+    }
+    else
+    {
+      try
+      {
+        it->second(std::cin, std::cout);
+      }
+      catch (const std::exception&)
+      {
+        std::cout << "<INVALID COMMAND>\n";
+        std::cin.ignore(std::numeric_limits< std::streamsize >::max(), '\n');
+      }
+    }
+  }
+}
