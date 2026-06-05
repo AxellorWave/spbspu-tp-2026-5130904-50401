c85333a9c5d4c2bccb22c2536dd8aeaf24a1d174
AxellorWave <Danya.Zharov.2006@gmail.com>
2026-06-05 17:51:50 +0300

    created polygon.hpp

---
+++ zharov.danil/T3/polygon.hpp
@@ -0,0 +1,23 @@
+#ifndef POLYGON_HPP
+#define POLYGON_HPP
+
+#include <vector>
+#include <iostream>
+
+namespace zharov
+{
+  struct Point
+  {
+    int x, y;
+  };
+
+  struct Polygon
+  {
+    std::vector< Point > points;
+  };
+
+  std::istream& operator>>(std::istream&, Point&);
+  std::istream& operator>>(std::istream&, Polygon&);
+}
+
+#endif
